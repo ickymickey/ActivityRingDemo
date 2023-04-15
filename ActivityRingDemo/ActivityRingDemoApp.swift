da2568838864a3c27487dad4fc25324d5c32e4e5
michayal <michayal411@gmail.com>
2023-04-15 15:27:05 -0600

    Initial Commit

---
+++ ActivityRingDemo/ActivityRingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityRingDemoApp.swift
+//  ActivityRingDemo
+//
+//  Created by Michayal Mathew on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ActivityRingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
